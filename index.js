45c54e29ba0c67df89d1a0c82cebc14b8bd3111e
acarik <acarik@gmail.com>
2019-10-14 00:17:04 +0300

    some trials

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const http = require("http");
 const PORT = process.env.PORT || 5000;
 const spawn = require("child_process").spawn;
+
+global.dummySayac = 0;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     var process = spawn('python', ["./myfun.py"]);
     process.stdout.on('data', function(data){
         pythonResponse = data.toString();
+        pythonResponse = pythonResponse + global.dummySayac.toString();
         res.end(pythonResponse);
     })
 });
@@ -14,7 +17,6 @@ server.listen(PORT, () => {
     console.log("Server running on " + PORT.toString())
 })
 
-global.dummySayac = 0;
 setInterval(myFunc, 1000);
 function myFunc(){
     console.log(global.dummySayac.toString());
